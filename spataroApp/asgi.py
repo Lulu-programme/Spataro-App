4f4e747937cb7eb759a1ac74ffaaa5d66cb718fd
Lulu-programme <lulu.programme@gmail.com>
2024-08-15 23:00:51 +0200

    Preparing the file and completing the README

---
+++ spataroApp/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for spataroApp project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spataroApp.settings')
+
+application = get_asgi_application()
